eaffb231e480f1864092f9fcacd39a6a600d52a9
Craftyryte <craftyryte2011@gmail.com>
2023-10-13 17:46:19 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Objectives:
+    1. Main, for running game, and Windows
+    2. Game class to create board and display icons
+    3. Logic class to handle all the checker logic etc
+    4. Details will be provided as documentation above the each class
+"""
